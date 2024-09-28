4734cd368956ba82c7890c33e239743e85592254
AbdullahBoreny <abdullah55boreny@gmail.com>
2024-09-29 00:35:00 +0300

    solving the first test case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value) {
+    const newArray =[];
+for(let i = 0; i <array.length; i++) {
+    if(array[i] === value) {
+        delete array[i];
+        
+    }
+    else {
+    newArray.push(array[i]);
+    }
+}
+return newArray;
+
 };
+removeFromArray([1, 2, 3, 4], 1,2,3); // should remove 3 and return [1,2,4]
+
+// Do not edit below this line
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
