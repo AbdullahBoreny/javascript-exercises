a84f5db3417cf0f7b6861516b9efd3193b7246bb
AbdullahBoreny <abdullah55boreny@gmail.com>
2024-09-29 21:20:10 +0300

    using Math.round method to round number to 1 decimal places

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let toCel = Math.round(((value - 32) * 5/9) *10) / 10;
+  return toCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let fah = (value * 9/5) + 32;
+  return fah;
 };
 
 // Do not edit below this line
